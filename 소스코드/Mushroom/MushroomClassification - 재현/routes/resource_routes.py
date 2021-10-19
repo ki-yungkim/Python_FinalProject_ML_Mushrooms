80cf26a96888f227f44d92b4481ffd85fc07c0df
takingtheback <84066112+takingtheback@users.noreply.github.com>
2021-10-19 18:54:52 +0900

    Update resource_routes.py

--- 소스코드/Mushroom/MushroomClassification - 재현/routes/resource_routes.py
+++ 소스코드/Mushroom/MushroomClassification - 재현/routes/resource_routes.py
@@ -42,42 +42,27 @@ def infoRequest():
 def testSurviveOrDeath():
     x = random.randrange(1, 6)
     url = "/imgQuestion/question" + str(x) + ".html"
-    # return render_template(url)
-    return render_template('/imgQuestion/question1.html')
+    return render_template(url)
+    # return render_template('/imgQuestion/question4.html')
 
 
-# 식용/독버섯 이미지 테스트
-@re_bp.route('/wrongAnswer')
+# 독버섯 정보
+@re_bp.route('/poisonousMushroom')
+def poisonousMushroom():
+    return render_template('/imgQuestion/poisonousMushroom.html')
+
+
+# 식용/독버섯 이미지 테스트: 오답페이지
+@re_bp.route('/wrongAnswer', methods=['POST'])
 def wrongAnswer():
-    advice = [
-        "버섯은 경고 표지를 달고 오지 않는다. 여러분은 입에 넣기 전에 손에 쥐고 있는 것이 무엇인지 알고 있어야 한다.",
-        "자문을 청하는 사람은 버섯에 중독되지 않는 사람이다.(남태평양 독립국 토가 속담)",
-        "의심나면 그대로 내버려두라!(If in doubt, leave it!)",
-        "의심나면 그냥 버려라!(If in doubt, discard it!)",
-        "버섯은 날로(生으로) 먹는 것이 아니고, 알코올음료와 함께 먹는 것도 아니며, 많이 먹는 것이 아니다.",
-        "식용버섯을 버리는 것이 독버섯을 먹고 중독되는 것보다 백번 낫다.",
-        "기억하라: 야생버섯 식용은 (클라이밍이나 스노우보드와 같은) 위험한 스포츠(extreme sport)도 아니고, 시식해 본 가장 긴 버섯목록 만들기 경쟁도 아니다.(Greg A. Marley)",
-        "모든 버섯은 먹을 수 있지만, 어떤 버섯들은 오직 단 한번만 먹을 수 있다.",
-        "늙은 버섯채취자와 용감한 버섯 채취자는 많지만 늙고 용감한 버섯 채취자는 없다"
-    ]
-    msg = random.choice(advice)
-    print(msg)
-    return render_template('/imgQuestion/wrongAnswer.html', msg=msg)
+    name = request.form['name']
+    msg = re_service.advice()
+    return render_template('/imgQuestion/wrongAnswer.html', msg=msg, name=name)
 
 
-@re_bp.route('/correctAnswer')
+# 식용/독버섯 이미지 테스트: 정답페이지
+@re_bp.route('/correctAnswer', methods=['POST'])
 def correctAnswer():
-    advice = [
-        "버섯은 경고 표지를 달고 오지 않는다. 여러분은 입에 넣기 전에 손에 쥐고 있는 것이 무엇인지 알고 있어야 한다.",
-        "자문을 청하는 사람은 버섯에 중독되지 않는 사람이다.(남태평양 독립국 토가 속담)",
-        "의심나면 그대로 내버려두라!(If in doubt, leave it!)",
-        "의심나면 그냥 버려라!(If in doubt, discard it!)",
-        "버섯은 날로(生으로) 먹는 것이 아니고, 알코올음료와 함께 먹는 것도 아니며, 많이 먹는 것이 아니다.",
-        "식용버섯을 버리는 것이 독버섯을 먹고 중독되는 것보다 백번 낫다.",
-        "기억하라: 야생버섯 식용은 (클라이밍이나 스노우보드와 같은) 위험한 스포츠(extreme sport)도 아니고, 시식해 본 가장 긴 버섯목록 만들기 경쟁도 아니다.(Greg A. Marley)",
-        "모든 버섯은 먹을 수 있지만, 어떤 버섯들은 오직 단 한번만 먹을 수 있다.",
-        "늙은 버섯채취자와 용감한 버섯 채취자는 많지만 늙고 용감한 버섯 채취자는 없다"
-    ]
-    msg = random.choice(advice)
-    print(msg)
-    return render_template('/imgQuestion/correctAnswer.html', msg=msg)
+    name = request.form['name2']
+    return render_template('/imgQuestion/correctAnswer.html', name=name)
+
