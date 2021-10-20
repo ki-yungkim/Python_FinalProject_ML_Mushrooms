53a76ca30398df383bbec8f7aeab4fe7e7c71d30
gruzam0615 <gruzam0615@gmail.com>
2021-10-20 18:22:05 +0900

    업로드된 이미지 처리 기능 추가

--- 소스코드/Mushroom/MushroomClassification - 박민재/app.py
+++ 소스코드/Mushroom/MushroomClassification - 박민재/app.py
@@ -1,27 +1,15 @@
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import joblib
 from flask import Flask, render_template
 # import routes
 from routes import route_mushroom
-from Mushroom.models import NetInfo
+
 app = Flask(__name__)
 
 # app.register_blueprint(routes.mushroom_bp)
 app.register_blueprint(route_mushroom.mushroom_bp)
 
-"""
-업로드된 이미지 파일 다루기 테스트
-"""
-UPLOAD_FOLDER = "static/uploads/"
-app.secret_key = "test"
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['MAX_CONTENT_LENGTH'] = 16 * 1280 * 1024
-
-@app.route('/')
-def root():
-    return render_template('index.html')
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
 class Net(nn.Module):
     def __init__(self):
@@ -43,6 +31,9 @@ class Net(nn.Module):
         x = self.fc3(x)
         return x
 
+@app.route('/')
+def root():
+    return render_template('index.html')
+
 if __name__ == "__main__":
     app.run()
-
---
+++ 소스코드/Mushroom/MushroomClassification - 박민재/routes/route_mushroom.py
@@ -0,0 +1,65 @@
+import os
+import urllib.request
+import requests
+from flask import *
+from models import model_mushrooms as mushroom
+from models import model_test as test
+
+mushroom_bp = Blueprint("mushroom", __name__, url_prefix="/mushroom")
+
+mushroom_service = mushroom.Service()
+test_service = test.Test01Service()
+
+@mushroom_bp.route("/test01")
+def test01():
+    return render_template("test/test01.html")
+
+@mushroom_bp.route("/test01_result", methods=["GET"])
+def test01Result():
+    st = request.args.get("st")
+    sw = request.args.get("sw")
+    numOfRows = request.args.get("numOfRows")
+    pageNo = request.args.get("pageNo")
+
+    print("GET:", st, sw, numOfRows, pageNo)
+
+    list = mushroom_service.Test01(st, sw, numOfRows, pageNo)
+
+    return render_template("test/test01result.html", list=list)
+
+@mushroom_bp.route("/test03")
+def test03():
+    return render_template("test/test03/test03.html")
+
+@mushroom_bp.route("/test03search_text", methods=["GET"])
+def test03input_A():
+    return render_template("test/test03/test03inputText.html")
+
+@mushroom_bp.route("/textSearch")
+def test03resultText():
+    return render_template("test/test03/test03result.html")
+
+@mushroom_bp.route("/test03search_image")
+def test03input_B():
+    print("Call test03search_image")
+    listdir = os.listdir("static/uploads/test/")
+
+    for i in range(len(listdir)):
+        print(listdir[i])
+        if listdir[i] != ".DS_Store":
+            os.remove("static/uploads/test/"+listdir[i])
+        else:
+            continue
+
+    return render_template("test/test03/test03inputImage.html")
+
+@mushroom_bp.route("/imageSearch", methods=["POST"])
+def test03resultImage():
+    inputImage = request.files["uploadfile"]
+    inputImage.save("static/uploads/test/"+(inputImage.filename))
+    print("Uploaded:", inputImage.filename)
+
+    res = test_service.getResult(inputImage)
+    print("Result:", res)
+
+    return render_template("test/test03/test03result_image.html", img=inputImage.filename, res=res)
