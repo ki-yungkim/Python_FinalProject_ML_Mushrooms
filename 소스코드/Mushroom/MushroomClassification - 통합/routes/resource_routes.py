3bce50d39a16deb14d83604b5eaf06f68915f75b
ki-yungkim <kiyungdev@gmail.com>
2021-10-22 11:47:41 +0900

    국문, 영문 분리, 페이징 기능 추가

--- 소스코드/Mushroom/MushroomClassification - 통합/routes/resource_routes.py
+++ 소스코드/Mushroom/MushroomClassification - 통합/routes/resource_routes.py
@@ -10,6 +10,11 @@ re_service = resource_model.Service()
 def resourceForm():
     return render_template('resourceForm.html')
 
+# 영문 입력 폼
+@re_bp.route('/resourceEngForm')
+def resourceEngForm():
+    return render_template('resourceEngForm.html')
+
 # 버섯도감 목록 검색
 @re_bp.route('/searchRequest', methods=['POST'])
 def searchRequest():
@@ -21,10 +26,22 @@ def searchRequest():
     numOfRows = request.form['numOfRows']
     # 페이지 번호
     pageNo = request.form['pageNo']
-
     List = re_service.searchRequest(st, sw, numOfRows, pageNo)
-    return render_template('searchRequest.html', List=List)
+    return render_template('searchRequest.html', List=List, sw=sw)
 
+# 버섯도감 목록 영문 검색
+@re_bp.route('/searchEngRequest', methods=['POST'])
+def searchEngRequest():
+    # 검색어 구분 (1 : 국명, 2 : 학명, 3 : 국명일치, 4 : 학명일치)
+    st = request.form['st']
+    # 검색어
+    sw = request.form['sw']
+    # 한 페이지 결과 수
+    numOfRows = request.form['numOfRows']
+    # 페이지 번호
+    pageNo = request.form['pageNo']
+    List = re_service.searchRequest(st, sw, numOfRows, pageNo)
+    return render_template('searchEngRequest.html', List=List, sw=sw)
 
 # 버섯도감 상세정보 조회
 @re_bp.route('/infoRequest', methods=['POST'])
