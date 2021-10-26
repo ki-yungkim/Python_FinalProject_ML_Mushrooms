252141b43f1d35b70e43f70f750f5532a44a3217
ki-yungkim <kiyungdev@gmail.com>
2021-10-26 10:03:23 +0900

    키 조정

--- 소스코드/기본템플릿-css적용/models/img_model.py
+++ 소스코드/기본템플릿-css적용/models/img_model.py
@@ -12,7 +12,7 @@ class ImageList:
 class Service:
     def __init__(self):
         self.base_url ='http://apis.data.go.kr/1390804/NihhsMushroomImageInfo/'
-        self.api_key = '0T%2F98gSX5j9sCWzfQv5sF20Bt3QHxB0k5iKt4tmI2lofZZemulH7eVuvEyF%2FhonmX4t1s%2Fdk3B%2FpmJ%2FmjoK9pA%3D%3D'
+        self.api_key = ''
 
     # 이미지 포함 검색
     def imgSearch(self, pageIndex, searchWord):
--- 소스코드/기본템플릿-css적용/models/resource_model.py
+++ 소스코드/기본템플릿-css적용/models/resource_model.py
@@ -22,7 +22,7 @@ class Resource:
 class Service:
     def __init__(self):
         self.base_url = 'http://openapi.nature.go.kr/openapi/service/rest/FungiService'
-        self.api_key = '0T%2F98gSX5j9sCWzfQv5sF20Bt3QHxB0k5iKt4tmI2lofZZemulH7eVuvEyF%2FhonmX4t1s%2Fdk3B%2FpmJ%2FmjoK9pA%3D%3D'
+        self.api_key = ''
 
     # 버섯도감 목록 검색
     def searchRequest(self, st, sw, numOfRows, pageNo):
