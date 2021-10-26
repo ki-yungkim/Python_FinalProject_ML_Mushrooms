d8257e26479a593c80e0febd57ecfa84eabccbde
ki-yungkim <kiyungdev@gmail.com>
2021-10-26 09:20:49 +0900

    특징으로 분석 결과 테이블 추가

--- 소스코드/Mushroom/MushroomClassification - 통합/routes/stand_out_route.py
+++ 소스코드/Mushroom/MushroomClassification - 통합/routes/stand_out_route.py
@@ -8,8 +8,14 @@ columns = stand_out_model.Columns
 # 입력 폼
 @co_bp.route('/standOutForm')
 def Form():
-
-    return render_template('standOutForm.html')
+    cap_shape = None
+    cap_surface = None
+    cap_color = None
+    bruises = None
+    odor = None
+    gill_color = None
+    population = None
+    return render_template('standOutForm.html', cap_shape=cap_shape, cap_surface=cap_surface, cap_color=cap_color,bruises=bruises,odor=odor, gill_color=gill_color,population=population)
 
 # 버섯도감 목록 검색
 @co_bp.route('/search', methods=['POST'])
@@ -27,5 +33,21 @@ def search():
         res = '독버섯'
     else:
         res = '독버섯 아님'
+    cap_shapeList = ['bell', 'conical', 'flat', 'knobbed', 'sunken', 'convex']
+    cap_surfaceList = ['fibrous', 'grooves', 'smooth', 'scaly']
+    cap_colorList = ['buff', 'cinnamon', 'red', 'gray', 'pink', 'green', 'purple', 'white', 'yellow']
+    bruisesList = ['없음', '있음']
+    odorList = ['almond', 'creosote', 'foul', 'anise', 'musty', 'none', 'pungent', 'spicy', 'fishy']
+    gill_colorList = ['buff', 'red', 'gray', 'chocolate', 'black', 'brown', 'orange', 'pink', 'green', 'purple', 'white','yellow']
+    populationList = ['abundant', 'clustered', 'numerous', 'scattered', 'several', 'solitary']
 
-    return str(res)
+    cap_shape = cap_shapeList[cap_shape]
+    cap_surface = cap_surfaceList[cap_surface-6]
+    cap_color = cap_colorList[cap_color-10]
+    bruises = bruisesList[bruises-20]
+    odor = odorList[odor-22]
+    gill_color = gill_colorList[gill_color-31]
+    population = populationList[population-43]
+
+
+    return render_template('standOutForm.html', res=res, cap_shape=cap_shape, cap_surface=cap_surface, cap_color=cap_color,bruises=bruises,odor=odor, gill_color=gill_color,population=population)
