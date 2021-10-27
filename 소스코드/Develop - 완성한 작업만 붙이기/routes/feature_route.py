76dd7ff87bc7a0a5cb58bf44134d44b5d06f0650
MoonSuin <84334574+MoonSuin@users.noreply.github.com>
2021-10-27 10:10:34 +0900

    feature update

--- 소스코드/Develop - 완성한 작업만 붙이기/routes/feature_route.py
+++ 소스코드/Develop - 완성한 작업만 붙이기/routes/feature_route.py
@@ -7,27 +7,51 @@ feature = feature_model.Feature
 
 # 입력 폼
 @fe_bp.route('/featureForm')
-def resourceForm():
-    return render_template('featureForm.html')
+def featureForm():
+    def Form():
+        cap_shape = None
+        cap_surface = None
+        cap_color = None
+        bruises = None
+        odor = None
+        gill_color = None
+        population = None
+        return render_template('featureForm.html', cap_shape=cap_shape, cap_surface=cap_surface, cap_color=cap_color,
+                               bruises=bruises, odor=odor, gill_color=gill_color, population=population)
 
-# 버섯도감 목록 검색
-@fe_bp.route('/search', methods=['POST'])
-def search():
-    bruises = int(request.form['bruises'])
-    odor = int(request.form['odor'])
-    gill_size = int(request.form['gill_size'])
-    gill_color = int(request.form['gill_color'])
-    stalk_surface_above_ring = int(request.form['stalk_surface_above_ring'])
-    stalk_surface_below_ring = int(request.form['stalk_surface_below_ring'])
-    ring_type = int(request.form['ring_type'])
-    spore_print_color =int(request.form['spore_print_color'])
-    population = int(request.form['population'])
-    f = [bruises, odor, gill_size, gill_color, stalk_surface_above_ring,\
-                                stalk_surface_below_ring, ring_type, spore_print_color, population]
-    res = fe_service.getResult(f)
-    if res == 1:
-        res = '독버섯'
-    else:
-        res = '독버섯 아님'
+    # 버섯도감 목록 검색
+    @co_bp.route('/search', methods=['POST'])
+    def search():
+        cap_shape = int(request.form['cap_shape'])
+        cap_surface = int(request.form['cap_surface'])
+        cap_color = int(request.form['cap_color'])
+        bruises = int(request.form['bruises'])
+        odor = int(request.form['odor'])
+        gill_color = int(request.form['gill_color'])
+        population = int(request.form['population'])
+        c = [cap_shape, cap_surface, cap_color, bruises, odor, gill_color, population]
+        res = co_service.getResult(c)
+        if res == 1:
+            res = '독버섯'
+        else:
+            res = '독버섯 아님'
+        cap_shapeList = ['bell', 'conical', 'flat', 'knobbed', 'sunken', 'convex']
+        cap_surfaceList = ['fibrous', 'grooves', 'smooth', 'scaly']
+        cap_colorList = ['buff', 'cinnamon', 'red', 'gray', 'pink', 'green', 'purple', 'white', 'yellow']
+        bruisesList = ['없음', '있음']
+        odorList = ['almond', 'creosote', 'foul', 'anise', 'musty', 'none', 'pungent', 'spicy', 'fishy']
+        gill_colorList = ['buff', 'red', 'gray', 'chocolate', 'black', 'brown', 'orange', 'pink', 'green', 'purple',
+                          'white', 'yellow']
+        populationList = ['abundant', 'clustered', 'numerous', 'scattered', 'several', 'solitary']
 
-    return str(res)
+        cap_shape = cap_shapeList[cap_shape]
+        cap_surface = cap_surfaceList[cap_surface - 6]
+        cap_color = cap_colorList[cap_color - 10]
+        bruises = bruisesList[bruises - 20]
+        odor = odorList[odor - 22]
+        gill_color = gill_colorList[gill_color - 31]
+        population = populationList[population - 43]
+
+        return render_template('featureForm.html', res=res, cap_shape=cap_shape, cap_surface=cap_surface,
+                               cap_color=cap_color, bruises=bruises, odor=odor, gill_color=gill_color,
+                               population=population)
