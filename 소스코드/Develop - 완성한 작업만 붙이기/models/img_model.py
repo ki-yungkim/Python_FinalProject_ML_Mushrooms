dc2b1321ca2f698eed76260e60c753b9f5906a88
rhdqngksmstkfka <gkdtkdrhdqnwnd@gmail.com>
2021-11-01 17:41:26 +0900

    코드 정리

--- 소스코드/Develop - 완성한 작업만 붙이기/models/img_model.py
+++ 소스코드/Develop - 완성한 작업만 붙이기/models/img_model.py
@@ -1,13 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
 
+
+
 class ImageList:
     def __init__(self, searchWord=None, pageIndex=None):
         # 검색어
         self.searchWord = searchWord
         # 선택페이지
         self.pageIndex = pageIndex
-
 
 class Service:
     def __init__(self):
@@ -27,14 +28,27 @@ class Service:
         if code == '1': # 출력부에서 받아오는 부분(응답메세지에서 가져오는)
             items = root.select('result')
             for item in items:
+                # 버섯명(국문)
                 scNameKr = item.find('scNameKr').get_text()
+                # 버섯명(학명)
                 scName = item.find('scName').get_text()
+                # 이미지 원본URL
                 imgFileUrl = item.find('imgFileUrl').get_text()
-                publishOrg = item.find('publishOrg').get_text()
 
-                results.append([scNameKr, scName, imgFileUrl, publishOrg])
+
+                results.append([scNameKr, scName, imgFileUrl])
             return results
 
         else:
             print('오류발생 code:', code)
             print('오류 메시지: ', resultMsg)
+
+
+
+
+
+
+
+
+
+
